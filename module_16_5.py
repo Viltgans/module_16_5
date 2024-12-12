2e69e0ed3db8c462ffc96b2c8c3c5b67a12905e1
Viltgans <80921674+Viltgans@users.noreply.github.com>
2024-12-12 07:48:25 +0300

    Update module_16_5.py

--- module_16_5.py
+++ module_16_5.py
@@ -34,21 +34,23 @@ async def post_user(username: Annotated[str, Path(min_length=5, max_length=20, d
 
 
 @app.put('/user/{user_id}/{username}/{age}')
-async def update_user(user_id: int, username: str, age: int) -> str:
+async def update_user(user_id: Annotated[int, Path(gt=0, lt=100, description='Enter User ID', example='1')],
+                      username: Annotated[str, Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser')],
+                    age: Annotated[int, Path(le=120, ge=18, description='Enter age', example='24')]) -> User:
     for user in users:
         if user.id == user_id:
             user.username = username
             user.age = age
-            return f"User {user_id} has been updated"
+            return user
     raise HTTPException(status_code=404, detail="User was not found")
 
 
 @app.delete('/user/{user_id}')
-async def delete_user(user_id: int) -> str:
+async def delete_user(user_id: int = Path(gt=0, lt=100, description='Enter User ID', example='1')) -> User:
     for exist_id, user in enumerate(users):
         if user.id == user_id:
             users.pop(exist_id)
-            return f'User {user_id} has been deleted'
+            return user
     raise HTTPException(status_code=404, detail="User was not found")
 
 ## Запуск:
